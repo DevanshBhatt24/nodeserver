8d80e79ffc3a7bf70bf44542f2f39504a4dfd2ea
DevanshBhatt <devanshbhatt10@gmail.com>
2023-09-11 16:41:13 +0530

    commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const appRouter=express.Router()
-const data=''
+
 appRouter.get('/',(req,res)=>{
   res.status(200).json('<h1>Hello</h1>')
 })
 
 appRouter.post('/location', (req,res)=>{
- data =req.body
+const data =req.body
     console.log(data)
   if(!data)
     return res.status(400).json({status:"failed"})
