ce2eec31599253161c96113ba60f7a35f0e31287
DevanshBhatt <devanshbhatt10@gmail.com>
2023-09-11 16:59:55 +0530

    commit

--- app.js
+++ app.js
@@ -14,4 +14,13 @@ const data =req.body
     return res.status(200).json({status:"recieved"})  
     
 })
+appRouter.post('/logs', (req,res)=>{
+  const data =req.body
+      console.log(data)
+    if(!data)
+      return res.status(400).json({status:"failed"})
+  
+      return res.status(200).json({status:"recieved"})  
+      
+  })
 module.exports=appRouter
