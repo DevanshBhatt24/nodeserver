47705d779812914ac71456517f40ec0cfe04b7a2
DevanshBhatt <devanshbhatt10@gmail.com>
2023-09-15 13:13:14 +0530

    new-url(logs)

--- app.js
+++ app.js
@@ -13,5 +13,13 @@ appRouter.post('/location', (req,res)=>{
       return res.status(200).json({status:"recieved"})  
       
   })
-
+  appRouter.post('/logs', (req,res)=>{
+    const data =req.body
+        console.log(data)
+      if(!data)
+        return res.status(400).json({status:"failed"})
+    
+        return res.status(200).json({status:"recieved"})  
+        
+    })
 module.exports=appRouter
