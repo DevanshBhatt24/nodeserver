63731b75f7db8a6c71dfec6e7ae0a83b32b7b064
DevanshBhatt <devanshbhatt10@gmail.com>
2024-01-31 15:46:38 +0530

    commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port= process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(appRouter)
 
 app.listen(port,"0.0.0.0" ,()=>console.log('Server has started on port:',port))
